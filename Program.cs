d619f43af556a4c7f25c4fdfde2fcec535f83f60
Arun Stephens <a@arun.nz>
2023-06-19 17:29:05 +1200

    AOT version that works

--- Program.cs
+++ Program.cs
@@ -2,6 +2,5 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-app.MapGet("/object", () => new { message = "Hello World" });
 
 app.Run();
